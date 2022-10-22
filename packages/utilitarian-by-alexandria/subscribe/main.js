c1e466cb180ff057bf419ca8170a56772ab3ecf8
Michael Francis <mikefrancis95@gmail.com>
2022-10-22 09:52:35 -0400

    Update main.js

--- packages/utilitarian-by-alexandria/subscribe/main.js
+++ packages/utilitarian-by-alexandria/subscribe/main.js
@@ -1,5 +1,18 @@
-exports.main = args => {
-  console.log(args);
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB);
+
+exports.main = async args => {
+  try {
+    await client.db('admin').collection('emails').insertOne({
+      email: args.email
+    });
+    console.log(`Successfully subscribed ${args.email}`);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
   return {
     body: 'Test'
   };
