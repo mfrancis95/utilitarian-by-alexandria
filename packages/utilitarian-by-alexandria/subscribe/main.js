d5e91bf70a6ac279686f6adcd6ad851cd4b3d25f
Michael Francis <4574480+mfrancis95@users.noreply.github.com>
2023-10-28 14:45:53 -0400

    Replace console.log usage with console.info and console.error

--- packages/utilitarian-by-alexandria/subscribe/main.js
+++ packages/utilitarian-by-alexandria/subscribe/main.js
@@ -5,13 +5,13 @@ const client = new MongoClient(process.env.MONGODB);
 exports.main = async args => {
   try {
     await client.connect();
-    console.log('Connected to database');
+    console.info('Connected to database');
     await client.db('admin').collection('emails').insertOne({
       email: args.email
     });
-    console.log(`Successfully subscribed ${args.email}`);
+    console.info(`Successfully subscribed ${args.email}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   } finally {
     await client.close();
   }
