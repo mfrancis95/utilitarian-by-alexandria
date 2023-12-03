47db2a56de0491461fa3abd95ff201bc58196c4e
Michael Francis <4574480+mfrancis95@users.noreply.github.com>
2023-12-02 20:52:42 -0500

    Add more logging statements

--- packages/utilitarian-by-alexandria/subscribe/main.js
+++ packages/utilitarian-by-alexandria/subscribe/main.js
@@ -4,8 +4,10 @@ const client = new MongoClient(process.env.MONGODB);
 
 exports.main = async args => {
   try {
+    console.info('Connecting to database');
     await client.connect();
     console.info('Connected to database');
+    console.info('Inserting email into database');
     await client.db('admin').collection('emails').insertOne({
       email: args.email
     });
@@ -13,7 +15,9 @@ exports.main = async args => {
   } catch (error) {
     console.error(error);
   } finally {
+    console.info('Closing database connection');
     await client.close();
+    console.info('Database connection closed');
   }
   return {
     headers: {
